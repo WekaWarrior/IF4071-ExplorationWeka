983535f7df2fed593fd1b050035f22f2d5c2fa68
Fahmi Dumadi <fah.midum.adi@gmail.com>
2015-09-26 19:04:34 +0700

    inisialisasi tugas weka

--- src/myclassifier/DT.java
+++ src/myclassifier/DT.java
@@ -8,7 +8,7 @@ package myclassifier;
 import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.trees.Id3;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 
@@ -16,34 +16,32 @@ import weka.core.SerializationHelper;
  *
  * @author Fahmi
  */
-public class naiveBayes {
-    Classifier NBClassifier;
+public class DT {
+    Classifier DTClassifier;
     Instances data;
-    public naiveBayes(){
+    public DT(){
         data = null;
-        NBClassifier = new NaiveBayes();
+        DTClassifier = new Id3();
     }
-    public naiveBayes(Instances data){
+    public DT(Instances data){
         this.data = data;
-        NBClassifier = new NaiveBayes();
+        DTClassifier = new Id3();
     }
     public void CrossValidation() throws Exception{
         Instances train = data;
         // train classifier
-        //Classifier cls = new NaiveBayes();
-        NBClassifier.buildClassifier(train);
-        //NBClassifier = cls;
+        DTClassifier.buildClassifier(train);
         // evaluate classifier and print some statistics
         Evaluation eval = new Evaluation(train);
-        eval.crossValidateModel(NBClassifier, train, 10, new Random(1));
+        eval.crossValidateModel(DTClassifier, train, 10, new Random(1));
         System.out.println(eval.toSummaryString("\nResults\n======\n", false));
         System.out.println(eval.toClassDetailsString("\n=== Detailed Accuracy By Class ===\n"));
         System.out.println(eval.toMatrixString());
     }
     public void SaveModel() throws Exception{
-        SerializationHelper.write("NaiveBayes.model", NBClassifier);
+        SerializationHelper.write("DT.model", DTClassifier);
     }
     public void LoadModel() throws Exception{
-        NBClassifier = (Classifier) SerializationHelper.read("NaiveBayes.model");
+        DTClassifier = (Classifier) SerializationHelper.read("DT.model");
     }
 }
